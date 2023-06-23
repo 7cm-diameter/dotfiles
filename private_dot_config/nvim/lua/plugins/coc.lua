e799896918fd41d34a836d9c481158690fac4315
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2023-06-23 14:18:43 +0900

    Bug fix

--- private_dot_config/nvim/lua/plugins/coc.lua
+++ private_dot_config/nvim/lua/plugins/coc.lua
@@ -7,13 +7,19 @@ return {
 
   config = function()
     local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
+
+    function _G.check_back_space()
+      local col = vim.fn.col('.') - 1
+      return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+    end
+
     -- set("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
     -- set("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
-    -- set("i", "<C-j>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
-    -- set("i", "<C-k>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+    set("i", "<C-j>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+    set("i", "<C-k>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
     set("i", "<C-space>", "coc#refresh()", { silent = true, expr = true })
     set("i", "<C-m>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
-    set("i", "<C-l>", "<Plug>(coc-snippets-expand)")
+    -- set("i", "<C-l>", "<Plug>(coc-snippets-expand)")
 
     set("n", "gn", "<Plug>(coc-diagnostic-prev)", { silent = true })
     set("n", "gp", "<Plug>(coc-diagnostic-next)", { silent = true })
