a226c6c344e31040eac70390df3de88e819d2a63
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2023-07-05 18:00:48 +0900

    Make `-d` to toggle `Diffview`

--- private_dot_config/nvim/lua/plugins/diffview.lua
+++ private_dot_config/nvim/lua/plugins/diffview.lua
@@ -2,6 +2,16 @@ return {
   "sindrets/diffview.nvim",
 
   config = function ()
-    require("diffview").setup({})
+    require("diffview").setup({
+      vim.keymap.set("n", "-d", "<CMD>DiffviewOpen<CR>"),
+      hooks = {
+        view_opened = function(view)
+          vim.keymap.set("n", "-d", "<CMD>DiffviewClose<CR>")
+        end,
+        view_closed = function(view)
+          vim.keymap.set("n", "-d", "<CMD>DiffviewOpen<CR>")
+        end,
+      }
+    })
   end
 }
